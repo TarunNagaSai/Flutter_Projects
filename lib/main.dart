0b9410a808db53e0ddfb1fcde8ce4abb1021776b
TarunNagaSai <tarunnagasai007@gmail.com>
2021-12-28 15:00:51 +0530

    New update 2.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:shopping/home.dart';
-import 'package:email_validator/email_validator.dart';
+import 'package:shopping/Screens/Cart/cart_screen.dart';
+import 'package:shopping/Screens/Home/home_screen.dart';
+import 'package:shopping/Screens/Login/login_screen.dart';
+import 'package:shopping/Services/login_services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Shopping());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Shopping extends StatefulWidget {
+  const Shopping({Key? key}) : super(key: key);
+
+  @override
+  _ShoppingState createState() => _ShoppingState();
+}
+
+class _ShoppingState extends State<Shopping> {
+  bool checkLogin = false;
+
+  @override
+  void initState() {
+    super.initState();
+    directPage();
+  }
+
+  void directPage() async {
+    bool check = false;
+    check = await LoginServices.checkLoginDetails();
+    setState(() {
+      checkLogin = check;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,111 +39,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Dog Food Products'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'Login with your email address',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                  ),
-                ),
-                const SizedBox(
-                  height: 16.0,
-                ),
-                TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Email Address',
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty || EmailValidator.validate(value) == false ) {
-                      return 'Enter a valid Email';
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-                const SizedBox(
-                  height: 16.0,
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Password',
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty || value.length < 6) {
-                      return 'Password should be atleast 6 charters long';
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-                const SizedBox(
-                  height: 16.0,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (context) {
-                          return const HomePage();
-                        }
-                      ),
-                      (route) => false);
-                    } else {
-
-                      //Snackbar Message for login page
-
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Invalid Form')),
-                      );
-
-                    }
-                  },
-                  child: const Text('Login'),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
+      initialRoute: checkLogin ? LoginScreen.id : HomePage.id,
+      routes: {
+        LoginScreen.id: (context) => const LoginScreen(),
+        HomePage.id: (context) => const HomePage(),
+        CartScreen.id: (context) => const CartScreen(),
+      },
     );
   }
 }
